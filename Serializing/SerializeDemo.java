5cb1220fde91964dcab6640627e86b685968432c
Andy93 <oneillandrew101@gmail.com>
2015-04-20 11:18:59 +0100

    Update SerializeDemo.java

--- Serializing/SerializeDemo.java
+++ Serializing/SerializeDemo.java
@@ -1,12 +1,10 @@
 import java.io.*;
-class Employee implements java.io.Serializable
-{
+class Employee implements java.io.Serializable{
    public String name;
    public String address;
    public transient int SSN;
    public int number;
-   public void mailCheck()
-   {
+   public void mailCheck(){
       System.out.println("Mailing a check to " + name
                            + " " + address);
    }
