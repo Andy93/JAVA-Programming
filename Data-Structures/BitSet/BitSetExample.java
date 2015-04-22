50e7c24c8c66bbe5422e0994c9e49c9e3b52c0b4
Andy93 <oneillandrew101@gmail.com>
2015-04-22 17:05:01 +0100

    Create BitSetExample.java

---
+++ Data-Structures/BitSet/BitSetExample.java
@@ -0,0 +1,32 @@
+import java.util.BitSet;
+public class BitSetExample{
+  public static void main(String args[]) {
+     BitSet b1 = new BitSet(16);
+     BitSet b2 = new BitSet(16);
+      
+     // set some bits
+     for(int i=0; i<50; i++) {
+        if((i%2) == 0) b1.set(i);
+        if((i%5) != 0) b2.set(i);
+     }
+     System.out.println("b1: ");
+     System.out.println(b1);
+     System.out.println("b2: ");
+     System.out.println(b2);
+
+     // AND bits
+     b2.and(b1);
+     System.out.println("\nb2 AND b1: ");
+     System.out.println(b2);
+
+     // OR bits
+     b2.or(b1);
+     System.out.println("\nb2 OR b1: ");
+     System.out.println(b2);
+
+     // XOR bits
+     b2.xor(b1);
+     System.out.println("\nb2 XOR b1: ");
+     System.out.println(b2);
+  }
+}
